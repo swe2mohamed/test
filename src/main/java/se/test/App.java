1b4640d0b1748018902b027b6d369d397b12333c
swe2mohamed <swe.mohamed1990@gmail.com>
2024-02-29 22:40:27 +0100

    Task 1

--- src/main/java/se/test/App.java
+++ src/main/java/se/test/App.java
@@ -4,10 +4,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        /*Objective: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+        //Objective: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
         System.out.println("Hello");
         System.out.println("Mohamed");
-        */
-
     }
 }
