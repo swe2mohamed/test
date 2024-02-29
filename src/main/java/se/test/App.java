149b4cfa95c84cbf2637ef9fb274a4222661b6c2
swe2mohamed <swe.mohamed1990@gmail.com>
2024-02-29 23:21:45 +0100

    Task 2: Objective: Write a Java program that performs basic arithmetic operations on two numbers provided by the user. The operations include addition, subtraction, multiplication, and division.

--- src/main/java/se/test/App.java
+++ src/main/java/se/test/App.java
@@ -1,10 +1,29 @@
 package se.test;
 
-public class App 
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello");
-        System.out.println("Mohamed");
+    /*
+    Task 2:
+    Objective: Write a Java program that performs basic arithmetic operations on two numbers provided by the user.
+    The operations include addition, subtraction, multiplication, and division.
+     */
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int secondNum = scanner.nextInt();
+        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
+        System.out.println(firstNum + " - " + secondNum + " = " + (firstNum-secondNum));
+        System.out.println(firstNum + " * " + secondNum + " = " + (firstNum*secondNum));
+        if (secondNum != 0){
+            System.out.println(firstNum + " / " + secondNum + " = " + ((double)firstNum/secondNum));
+        }
+        else {
+            System.out.println("You can not divide by zero");
+        }
     }
 }
