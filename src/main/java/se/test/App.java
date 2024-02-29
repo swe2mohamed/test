3e1e1b8bfc43cb2b96a97d74e01b7306363e9e73
swe2mohamed <swe.mohamed1990@gmail.com>
2024-02-29 22:38:15 +0100

    Initial commit

---
+++ src/main/java/se/test/App.java
@@ -0,0 +1,13 @@
+package se.test;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        /*Objective: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+        System.out.println("Hello");
+        System.out.println("Mohamed");
+        */
+
+    }
+}
