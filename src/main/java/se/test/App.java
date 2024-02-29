6025228f2f6aa01ada481643c4ae29f6a5cb29e5
swe2mohamed <swe.mohamed1990@gmail.com>
2024-03-01 00:20:56 +0100

    Task 3: Exercise: Check Even or Odd Objective: Write a Java program that asks the user to enter a number and then prints whether the number is even or odd.

--- src/main/java/se/test/App.java
+++ src/main/java/se/test/App.java
@@ -7,23 +7,19 @@ public class App
     public static void main( String[] args )
     {
     /*
-    Task 2:
-    Objective: Write a Java program that performs basic arithmetic operations on two numbers provided by the user.
-    The operations include addition, subtraction, multiplication, and division.
-     */
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        int firstNum = scanner.nextInt();
-        System.out.print("Enter second number: ");
-        int secondNum = scanner.nextInt();
-        System.out.println(firstNum + " + " + secondNum + " = " + (firstNum+secondNum));
-        System.out.println(firstNum + " - " + secondNum + " = " + (firstNum-secondNum));
-        System.out.println(firstNum + " * " + secondNum + " = " + (firstNum*secondNum));
-        if (secondNum != 0){
-            System.out.println(firstNum + " / " + secondNum + " = " + ((double)firstNum/secondNum));
-        }
-        else {
-            System.out.println("You can not divide by zero");
-        }
+    Task 3:
+    Exercise: Check Even or Odd
+    Objective: Write a Java program that asks the user to enter a number and then prints whether the number
+    is even or odd.
+    */
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int num = scanner.nextInt();
+    if (num % 2 == 0){
+        System.out.println(num + " is even.");
+    }
+    else {
+        System.out.println(num + " is odd.");
+    }
     }
 }
