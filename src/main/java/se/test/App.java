2bcd2562d6ba3985149b5dfdfb99eb86d16ae749
swe2mohamed <swe.mohamed1990@gmail.com>
2024-02-29 22:41:21 +0100

    Task 1: Objective: Write a Java program to print 'Hello' on screen and then print your name on a separate line.

--- src/main/java/se/test/App.java
+++ src/main/java/se/test/App.java
@@ -4,7 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Objective: Write a Java program to print 'Hello' on screen and then print your name on a separate line.
         System.out.println("Hello");
         System.out.println("Mohamed");
     }
